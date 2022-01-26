0de271742f9a6bd3e2c32f149d2be69e9849c001
Alex Krueger <alexckrueger@gmail.com>
2022-01-26 16:42:07 -0600

    adds authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
+  get "/user_info" => "users#info"
 end
